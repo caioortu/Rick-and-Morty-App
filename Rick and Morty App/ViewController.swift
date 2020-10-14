63ef82c76efa4cb89f15ab7b13d664a9d730abd7
Caio Ortu <caio.ortu@zup.com.br>
2020-10-14 13:06:38 -0300

    Initial Commit

---
+++ Rick and Morty App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty App
+//
+//  Created by Caio Ortu on 10/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
